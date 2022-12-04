9a3adb93929477d462c583cd1824fdaa097d5610
Buczkek <jakub.buczek@skni.umcs.pl>
2022-12-04 02:28:48 +0100

    Basic implementation of jwt authorization

---
+++ src/auth/security.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timedelta
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+from .models import TokenData
+from .exceptions import InvalidCredentialsException, InactiveUserException
+
+from src.user.service import get_by_username
+from src.user.exceptions import UserNotFoundException
+from src.user.models import User
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = "de50745a703fbb62285736c1038ac45434f8ec90f20233c2586c22590e628d15"  # openssl rand -hex 32
+ALGORITHM = "HS256"
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="user/token")  # placeholder for a real url
+
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password) -> str:
+    return pwd_context.hash(password)
+
+
+def authenticate_user(username: str, password: str) -> User | None:
+    try:
+        user = get_by_username(username)
+        # user = get_user(fake_db, username)
+    except UserNotFoundException:
+        return None
+
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    exc = InvalidCredentialsException(headers={"WWW-Authenticate": "Bearer"})
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise exc
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise exc
+    user = get_by_username(username=token_data.username)
+    if user is None:
+        raise exc
+    return user
+
+
+async def get_active_user(current_user: User = Depends(get_current_user)):
+    if current_user is None:
+        raise InactiveUserException()
+    return current_user
