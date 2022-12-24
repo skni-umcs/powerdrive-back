7c088ad18668a826fea5ffb45df15cdc59b2736b
jan.bylina <jan.bylina@skni.umcs.pl>
2022-12-24 14:36:19 +0100

    Add tests in docker container

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
+TYPE=$1
+PORT=8000
+
 python setup.py
-python main.py
+
+if [ "$TYPE" = "dev" ]; then
+    echo "Running in development mode"
+    uvicorn src.rest:app --reload --host 0.0.0.0 --port "$PORT"
+elif [ "$TYPE" = test ] || [ "$TYPE" = pytest ]; then
+    echo "Running in test mode"
+    pytest
+elif [ "$TYPE" = "prod" ]; then
+    echo "Running in production mode"
+    gunicorn --workers 4 -k uvicorn.workers.UvicornWorker rest.rest:app --bind 0.0.0.0:"$PORT"
+fi
+#python main.py
