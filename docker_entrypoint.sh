d6303063c27ed23c6c9c81ec5253ed0bd03f5c44
jan.bylina <jan.bylina@skni.umcs.pl>
2023-01-04 23:17:32 +0100

    Small fixes in docker tests

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,30 +3,27 @@
 TYPE=$1
 PORT=8000
 
-python setup.py
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+BRED='\033[1;31m'
+NC='\033[0m' # No Color
 
 if [ "$TYPE" = "dev" ]; then
-    echo "Running in development mode"
+    echo -e "${CYAN}Running in development mode${NC}"
+    python setup.py dev
     uvicorn src.api:app --reload --host 0.0.0.0 --port "$PORT"
 
 elif [ "$TYPE" = test ] || [ "$TYPE" = pytest ]; then
-    echo "Setup test database"
+
+
     export DB_NAME=powerdrive_test
-    export DB_USER=powerdrive
-    export DB_PASSWORD=powerdrive
-    export DB_HOST=db
-    export DB_PORT=5432
-    export DB_TEST_NAME=powerdrive_test
-    export DB_URL=postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME
-#    export DB_URL="postgresql://postgres:postgres@db:5432/$DB_NAME"
     python setup.py test
-    echo "Running tests"
+
+    echo -e "${CYAN}Running tests...${NC}"
     pytest
-    echo "$DB_NAME"
-    export
 
 elif [ "$TYPE" = "prod" ]; then
-    echo "What do you mean production mode XD"
+    echo -e "${BRED}What do you mean production mode XD${NC}"
 #    echo "Running in production mode"
 #    gunicorn --workers 4 -k uvicorn.workers.UvicornWorker src.api:app --bind 0.0.0.0:"$PORT"
 fi
