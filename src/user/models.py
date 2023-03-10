c5d8ff657b519ea293f2527b5e4fd116cd8f24da
kingastec <kingastec439@gmail.com>
2023-03-10 08:22:02 +0100

    changes in user

--- src/user/models.py
+++ src/user/models.py
@@ -15,7 +15,7 @@ def get_password_hash(password) -> str:
     return pwd_context.hash(password)
 
 
-class DBUser(Base):
+class User(Base):
     __tablename__ = 'User'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
