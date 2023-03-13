6e4800aa1be62a7ad0cf6d44b3fc47307c290adb
kingastec <kingastec439@gmail.com>
2023-03-13 22:59:50 +0100

    add group with current user

--- src/group/service.py
+++ src/group/service.py
@@ -1,111 +1,174 @@
 from src.group.exceptions import GroupNotFoundException, OwnerNotFoundException, \
     GroupNameAlreadyExistsException, UserNotFoundException, NoGroupPermissionsException
 from sqlalchemy.orm import Session
-from src.group.models import DBGroup, DBGroupUser
-from src.group.schemas import GroupCreate, GroupUpdate, Group
-from src.user.service import get_by_index
+from src.group.models import Group, GroupUser
+from src.group.schemas import GroupCreate, GroupUpdate
+from src.user.service import get_by_index as get_user_by_index
 from src.user.models import User
+from fastapi import HTTPException
 
 
-def add_group(group: GroupCreate, session: Session, owner_id: int) -> DBGroup:
+
+def if_current_can_manipulate_group(session: Session, group_id: int, current_user_id: int) -> bool:
+    if get_user_by_index(session=session, user_id=current_user_id).if_admin:
+        return True
+    db_group = session.query(Group).filter(Group.group_owner_id == current_user_id).first()
+    if not db_group:
+            return False
+    if db_group.group_owner_id == current_user_id:
+        return True
+    return False
+
+
+def if_current_can_see_group(session: Session, group_id: int, current_user_id: int) -> bool:
+    if get_user_by_index(session=session, user_id=current_user_id).if_admin:
+        return True
+    if session.query(GroupUser).filter(GroupUser.group_id == group_id and GroupUser.user_id == current_user_id).first():
+        return True
+    return False
+
+
+def add_group(group: GroupCreate, session: Session, owner_id: int) -> Group:
 
     if get_group_by_name(session=session, group_name=group.group_name):
         raise GroupNameAlreadyExistsException()
-    db_owner = get_by_index(session=session, user_id=owner_id)
-    if not db_owner:
-        raise OwnerNotFoundException()
 
-    created_group = DBGroup(group_name=group.group_name,
-                            group_description=group.group_description,
-                            group_owner_id=db_owner.id)
+    created_group = Group(group_name=group.group_name,
+                          group_description=group.group_description,
+                          group_owner_id=owner_id)
     session.add(created_group)
     session.commit()
     return created_group
 
 
-def get_all_groups(session: Session,) -> list[DBGroup]:
-    groups = session.query(DBGroup).all()
-    return groups
-
-
-def get_group_by_index(session: Session, group_id: int) -> DBGroup:
-    group = session.query(DBGroup).filter(DBGroup.group_id == group_id).first()
-
-    if not group:
-        raise GroupNotFoundException()
-
+# def get_all_groups(session: Session, current_user_id: int) -> list[Group]:
+#     if get_user_by_index(session=session, user_id=current_user_id).if_admin:
+#         groups = session.query(Group).all()
+#         if not groups:
+#             raise GroupNotFoundException()
+#         return groups
+#
+#     db_group_for_user1 = session.query(GroupUser).filter(GroupUser.user_id == current_user_id).all()
+#     db_group_for_user2 = session.query(Group).filter(Group.group_owner_id == current_user_id).all()
+#
+#     index1 = [g.user_id for g in db_group_for_user1]
+#     index2 = [g.group_id for g in db_group_for_user2]
+#     index = index1 + index2
+#     unique_index = []
+#     [unique_index.append(i) for i in index if i not in unique_index]
+#     groups = [get_group_by_index(session, g, current_user_id) for g in unique_index]
+#     if not groups:
+#         raise GroupNotFoundException()
+#     return groups
+#
+#
+#
+# # def get_by_index(session: Session, group_id: int, current_user_id: int) -> Group:
+# #     group = session.query(Group).filter(Group.group_id == group_id).first()
+# #
+# #     if not group:
+# #         raise GroupNotFoundException()
+# #
+# #     if group.group_owner_id != current_user_id \
+# #             or not get_by_index(session=session, user_id=current_user_id).if_admin:
+# #         raise HTTPException(status_code=401, detail=str("No group permission"))
+# #
+# #     return group
+# def get_group_by_index(session: Session, group_id: int, current_user_id: int) -> Group:
+#     group = session.query(Group).filter(Group.group_id == group_id).first()
+#
+#     if not group:
+#         raise GroupNotFoundException()
+#
+#     if group.group_owner_id != current_user_id \
+#             or not get_user_by_index(session=session, user_id=current_user_id).if_admin:
+#         raise HTTPException(status_code=401, detail=str("No group permission"))
+#
+#     return group
+#
+#
+# def get_group_users(session: Session, group_id: int, current_user_id: int) -> list[User]:
+#     group = session.query(Group).filter(Group.group_id == group_id).first()
+#
+#     if not group:
+#         raise GroupNotFoundException()
+#
+#     if group.group_owner_id != current_user_id \
+#             or not get_user_by_index(session=session, user_id=current_user_id).if_admin:
+#         raise HTTPException(status_code=401, detail=str("No group permission"))
+#
+#     users = []
+#     group_users = session.query(GroupUser).filter(group_id == group_id).all()
+#     for gu in group_users:
+#         u = get_user_by_index(session=session, user_id=gu.user_id)
+#         if not u:
+#             raise UserNotFoundException()
+#         users.append(u)
+#     return users
+#
+#
+def get_group_by_name(session: Session, group_name: str) -> Group:
+    group = session.query(Group).filter(Group.group_name == group_name).first()
     return group
-
-
-def get_group_users(session: Session, group_id: int) -> list[User]:
-    group = session.query(DBGroup).filter(DBGroup.group_id == group_id).first()
-
-    if not group:
-        raise GroupNotFoundException()
-
-    users = []
-    group_users = session.query(DBGroupUser).filter(group_id == group_id).all()
-    for gu in group_users:
-        u = get_by_index(session=session, user_id=gu.user_id)
-        if not u:
-            raise UserNotFoundException()
-        users.append(u)
-    return users
-
-
-def get_group_by_name(session: Session, group_name: str) -> DBGroup:
-    group = session.query(DBGroup).filter(DBGroup.group_name == group_name).first()
-    return group
-
-
-def update_group_by_index(session: Session, incoming_group: GroupUpdate, current_user_id: int) -> DBGroup:
-    db_group = get_group_by_index(session=session, group_id=incoming_group.group_id)
-    if not db_group:
-        raise GroupNotFoundException
-    if db_group.group_owner_id != current_user_id:
-        raise NoGroupPermissionsException()
-    if incoming_group.group_description:
-        db_group.group_description = incoming_group.group_description
-
-    session.commit()
-    return db_group
-
-
-def add_user_to_group(session: Session, group: Group, new_user_id: int, current_user_id: int) -> DBGroupUser:
-    if get_by_index(session=session, user_id=group.group_owner_id) != get_by_index(session=session,
-                                                                                             user_id=current_user_id):
-        raise NoGroupPermissionsException()
-
-    db_new_user = get_by_index(session=session, user_id=new_user_id)
-    if not db_new_user:
-        raise UserNotFoundException()
-    group_user = DBGroupUser(group_id=group.group_id, user_id=db_new_user.id)
-    session.add(group_user)
-    session.commit()
-    return group_user
-
-
-def delete_user_from_group(session: Session, group_id: int, user_id: int, current_user_id: int) -> None:
-    group = get_group_by_index(session=session, group_id=group_id)
-    if not group:
-        raise GroupNotFoundException
-    if get_by_index(session=session, user_id=group.group_owner_id) != get_by_index(session=session,
-                                                                                   user_id=current_user_id):
-        raise NoGroupPermissionsException()
-    group_user = session.query(DBGroupUser).filter(DBGroupUser.group_id == group_id
-                                                   and DBGroupUser.user_id == user_id).first()
-    session.delete(group_user)
-    session.commit()
-
-
-def delete_group_by_index(session: Session, group_id: int, current_user_id: int) -> None:
-    group = session.query(DBGroup).filter(DBGroup.group_id == group_id).first()
-    if get_by_index(session=session, user_id=group.group_owner_id) != get_by_index(session=session,
-                                                                                       user_id=current_user_id):
-        raise NoGroupPermissionsException()
-
-    if not group:
-        raise GroupNotFoundException()
-
-    session.delete(group)
-    session.commit()
+#
+#
+# def update_group_by_index(session: Session, incoming_group: GroupUpdate, current_user_id: int) -> Group:
+#     db_group = get_group_by_index(session=session, group_id=incoming_group.group_id, current_user_id=current_user_id)
+#
+#     if not db_group:
+#         raise GroupNotFoundException
+#
+#     if db_group.group_owner_id != current_user_id \
+#             or not get_user_by_index(session=session, user_id=current_user_id).if_admin:
+#         raise HTTPException(status_code=401, detail=str("No group permission"))
+#
+#     if incoming_group.group_description:
+#         db_group.group_description = incoming_group.group_description
+#
+#     session.commit()
+#     return db_group
+#
+#
+# def add_user_to_group(session: Session, group_id: int, new_user_id: int, current_user_id: int) -> GroupUser:
+#     db_group = get_group_by_index(session=session, group_id=group_id, current_user_id=current_user_id)
+#
+#     if not db_group:
+#         raise GroupNotFoundException
+#
+#     if db_group.group_owner_id != current_user_id \
+#             or get_user_by_index(session=session, user_id=current_user_id).if_admin == False:
+#         raise HTTPException(status_code=401, detail=str("No group permission"))
+#
+#     db_new_user = get_user_by_index(session=session, user_id=new_user_id)
+#     if not db_new_user:
+#         raise UserNotFoundException()
+#     group_user = GroupUser(group_id=group_id, user_id=new_user_id)
+#     session.add(group_user)
+#     session.commit()
+#     return group_user
+#
+#
+# def delete_user_from_group(session: Session, group_id: int, user_id: int, current_user_id: int) -> None:
+#     db_group = get_group_by_index(session=session, group_id=group_id, current_user_id=current_user_id)
+#
+#     if user_id == current_user_id:
+#         db_group = session.query(GroupUser).filter(Group.group_id == group_id).first()
+#
+#     if not db_group:
+#         raise GroupNotFoundException
+#
+#
+#     group_user = session.query(GroupUser).filter(GroupUser.group_id == group_id
+#                                                  and GroupUser.user_id == user_id).first()
+#     session.delete(group_user)
+#     session.commit()
+#
+#
+# def delete_group_by_index(session: Session, group_id: int, current_user_id: int) -> None:
+#     db_group = get_group_by_index(session=session, group_id=group_id, current_user_id=current_user_id)
+#
+#     if not db_group:
+#         raise GroupNotFoundException
+#
+#     session.delete(db_group)
+#     session.commit()
--- src/group/views.py
+++ src/group/views.py
@@ -5,85 +5,77 @@ from src.group import service
 from src.group.schemas import Group, GroupCreate, GroupUpdate, GroupUser
 from src.user.schemas import User
 from src.dependencies import get_db
-
-from src.user.service import get_by_index
+from src.auth.security import get_current_user
 
 api_router = APIRouter(prefix="/group", tags=["group"])
 
 
 @api_router.post("/", response_model=Group)
-async def add_group(group: GroupCreate, current_user: User, db: Session = Depends(get_db)):
+async def add_group(group: GroupCreate, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
-        db_current_user = get_by_index(session=db, user_id=current_user.id)
-    except OwnerNotFoundException as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    try:
-        new_group = service.add_group(group=group, session=db, owner_id=db_current_user.id)
+        new_group = service.add_group(group=group, session=db, owner_id=current_user.id)
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
     return new_group
 
 
-@api_router.post("/{group_id}/add_user", response_model=GroupUser)
-async def add_user_to_group(group_id: int, user: User, current_user: User, db: Session = Depends(get_db)):
-    try:
-        db_current_user = get_by_index(session=db, user_id=current_user.id)
-    except OwnerNotFoundException as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    try:
-        new_group_user = service.add_user_to_group(session=db,
-                                                   group=service.get_group_by_index(session=db, group_id=group_id),
-                                                   new_user_id=user.id, current_user_id=db_current_user.id)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    return new_group_user
-
-
-@api_router.get("/", response_model=list[Group])
-async def get_all_groups(db: Session = Depends(get_db)):
-    return service.get_all_groups(db)
-
-
-@api_router.get("/{group_id}", response_model=Group)
-async def get_group(group_id: int, db: Session = Depends(get_db)):
-    try:
-        group = service.get_group_by_index(session=db, group_id=group_id)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    return group
-
-
-@api_router.get("/{group_id}/users", response_model=list[User])
-async def get_group_users(group_id: int, db: Session = Depends(get_db)):
-    return service.get_group_users(session=db, group_id=group_id)
-
-
-@api_router.put("/{group_id}", response_model=Group)
-async def update_group(incoming_group: GroupUpdate, current_user: User, db: Session = Depends(get_db)):
-    db_current_user = get_by_index(session=db, user_id=current_user.id)
-    try:
-        updated_group = service.update_group_by_index(session=db, incoming_group=incoming_group,
-                                                      current_user_id=db_current_user.id)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
-    return updated_group
-
-
-@api_router.delete("/{group_id}", status_code=204)
-async def delete_group(group_id: int, current_user: User, db: Session = Depends(get_db)):
-    db_current_user = get_by_index(session=db, user_id=current_user.id)
-    try:
-        service.delete_group_by_index(session=db, group_id=group_id, current_user_id=db_current_user.id)
-    except GroupNotFoundException as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
-
-@api_router.delete("/{group_id}/delete_user/{user_id}", status_code=204)
-async def delete_user_from_group(group_id: int, user_id: int, current_user: User, db: Session = Depends(get_db)):
-    db_current_user = get_by_index(session=db, user_id=current_user.id)
-    try:
-        service.delete_user_from_group(session=db, group_id=group_id, user_id=user_id,
-                                       current_user_id=db_current_user.id)
-    except GroupNotFoundException as e:
-        raise HTTPException(status_code=404, detail=str(e))
+# @api_router.post("/{group_id}/add_user", response_model=GroupUser)
+# async def add_user_to_group(group_id: int, user: User,
+#                             current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+#     try:
+#         new_group_user = service.add_user_to_group(session=db,
+#                                                    group_id=group_id,
+#                                                    new_user_id=user.id, current_user_id=current_user.id)
+#     except Exception as e:
+#         raise HTTPException(status_code=404, detail=str(e))
+#     return new_group_user
+#
+#
+# @api_router.get("/", response_model=list[Group])
+# async def get_all_groups(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+#     return service.get_all_groups(db, current_user.id)
+#
+#
+# @api_router.get("/{group_id}", response_model=Group)
+# async def get_group(group_id: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+#
+#     group = service.get_by_index(session=db, group_id=group_id, current_user_id=current_user.id)
+#     if not group:
+#     # except Exception as e:
+#         raise HTTPException(status_code=404, detail=str("Group not found"))
+#     return group
+#
+#
+# @api_router.get("/{group_id}/users", response_model=list[User])
+# async def get_group_users(group_id: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+#     return service.get_group_users(session=db, group_id=group_id, current_user_id=current_user.id)
+#
+#
+# @api_router.put("/{group_id}", response_model=Group)
+# async def update_group(incoming_group: GroupUpdate, current_user: User = Depends(get_current_user),
+#                        db: Session = Depends(get_db)):
+#     try:
+#         updated_group = service.update_group_by_index(session=db, incoming_group=incoming_group,
+#                                                       current_user_id=current_user.id)
+#     except Exception as e:
+#         raise HTTPException(status_code=404, detail=str(e))
+#
+#     return updated_group
+#
+#
+# @api_router.delete("/{group_id}", status_code=204)
+# async def delete_group(group_id: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+#     try:
+#         service.delete_group_by_index(session=db, group_id=group_id, current_user_id=current_user.id)
+#     except GroupNotFoundException as e:
+#         raise HTTPException(status_code=404, detail=str(e))
+#
+#
+# @api_router.delete("/{group_id}/delete_user/{user_id}", status_code=204)
+# async def delete_user_from_group(group_id: int, user_id: int, current_user: User = Depends(get_current_user),
+#                                  db: Session = Depends(get_db)):
+#     try:
+#         service.delete_user_from_group(session=db, group_id=group_id, user_id=user_id,
+#                                        current_user_id=current_user.id)
+#     except GroupNotFoundException as e:
+#         raise HTTPException(status_code=404, detail=str(e))
