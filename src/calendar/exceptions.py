0ef454a33928336f7c81c60c7e752d2d73c87435
Jakub Buczek <99tuba99@gmail.com>
2023-03-20 18:28:23 +0100

    Added exceptions for calendar and events, corrected date types in schemas, added some services, created endpoints for basic manipulations

--- src/calendar/exceptions.py
+++ src/calendar/exceptions.py
@@ -17,3 +17,33 @@ class CalendarNameTakenException(CalendarException):
     """Raised when the calendar name is already taken."""
     def __init__(self, message: str = "Calendar name already taken", status_code: int = status.HTTP_400_BAD_REQUEST, headers: dict = None):
         super().__init__(message=message, status_code=status_code, headers=headers)
+
+
+class UnauthorizedCalendarException(CalendarException):
+    """Raised when the user is not authorized to access the calendar."""
+    def __init__(self, message: str = "Unauthorized calendar", status_code: int = status.HTTP_401_UNAUTHORIZED, headers: dict = None):
+        super().__init__(message=message, status_code=status_code, headers=headers)
+
+
+class EventException(HTTPException):
+    """Base class for all exceptions raised by the event module."""
+    def __init__(self, message: str, status_code: int = status.HTTP_400_BAD_REQUEST, headers: dict = None):
+        super().__init__(status_code=status_code, detail=message, headers=headers)
+
+
+class EventNotFoundException(EventException):
+    """Raised when the event is not found."""
+    def __init__(self, message: str = "Event not found", status_code: int = status.HTTP_404_NOT_FOUND, headers: dict = None):
+        super().__init__(message=message, status_code=status_code, headers=headers)
+
+
+class UnauthorizedEventException(EventException):
+    """Raised when the user is not authorized to access the event."""
+    def __init__(self, message: str = "Unauthorized event", status_code: int = status.HTTP_401_UNAUTHORIZED, headers: dict = None):
+        super().__init__(message=message, status_code=status_code, headers=headers)
+
+
+class EventAlreadyExistsException(EventException):
+    """Raised when the event already exists."""
+    def __init__(self, message: str = "Event already exists", status_code: int = status.HTTP_400_BAD_REQUEST, headers: dict = None):
+        super().__init__(message=message, status_code=status_code, headers=headers)
