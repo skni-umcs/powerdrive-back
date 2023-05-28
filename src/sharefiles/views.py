24ab2037ddd9f6c9e93943f02eac49e2b90c2b8f
kingastec <kingastec439@gmail.com>
2023-05-28 22:59:42 +0200

    fix router

--- src/sharefiles/views.py
+++ src/sharefiles/views.py
@@ -13,12 +13,31 @@ api_router = APIRouter(prefix="/share/file", tags=["sharefile"])
 
 
 @api_router.post("/", response_model=ShareFileUser)
-def add_share(share: ShareFileUserCreate, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+def add_share(share: ShareFileUserCreate, current_user: User = Depends(get_current_user),
+              db: Session = Depends(get_db)):
     try:
         return service.add(current_user=current_user, share=share, db=db)
     except NotAuthorizedShareFileException as e:
         raise HTTPException(status_code=401, detail=str(e))
     except FileNotFoundException as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+
+@api_router.get("/me", response_model=list[ShareFileUser])
+async def get_all_for_user(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return service.get_all_for_user(current_user=current_user, db=db)
+
+
+@api_router.put("/", response_model=ShareFileUser)
+async def update_share(share: ShareFileUserUpdate,
+                 current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    try:
+        return service.update(current_user=current_user, share=share, db=db)
+    except NotAuthorizedShareFileException as e:
+        raise HTTPException(status_code=401, detail=str(e))
+    except FileNotFoundException as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except ShareFileNotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
 
 
@@ -33,29 +52,12 @@ async def get_share(share_id: int, current_user: User = Depends(get_current_user
     except ShareFileNotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
 
-@api_router.get("/me", response_model=list[ShareFileUser])
-async def get_all_for_user(current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    return service.get_all_for_user(current_user=current_user, db=db)
-
 
 @api_router.get("/forfile/{file_id}/", response_model=list[ShareFileUser])
 async def get_all_for_file(file_id: int,current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
         return service.get_all_for_file(file_id=file_id,current_user=current_user, db=db)
     except FileNotFoundException as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
-
-@api_router.put("/", response_model=ShareFileUser)
-async def update_share(share: ShareFileUserUpdate,
-                 current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    try:
-        return service.update(current_user=current_user, share=share, db=db)
-    except NotAuthorizedShareFileException as e:
-        raise HTTPException(status_code=401, detail=str(e))
-    except FileNotFoundException as e:
-        raise HTTPException(status_code=404, detail=str(e))
-    except ShareFileNotFoundException as e:
         raise HTTPException(status_code=404, detail=str(e))
 
 
