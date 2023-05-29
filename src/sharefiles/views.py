68dbf921d4f0f758657e53f7e3a194cf9f086f40
Michał Latra <michal.latra@gmail.com>
2023-05-29 09:01:02 +0200

    Fix folder sharing routes

--- src/sharefiles/views.py
+++ src/sharefiles/views.py
@@ -12,7 +12,7 @@ from src.sharefiles.exceptions import NotAuthorizedShareFileException, FileNotFo
 api_router = APIRouter(prefix="/share/file", tags=["sharefile"])
 
 
-@api_router.post("/", response_model=ShareFileUser)
+@api_router.post("", response_model=ShareFileUser)
 def add_share(share: ShareFileUserCreate, current_user: User = Depends(get_current_user),
               db: Session = Depends(get_db)):
     try:
@@ -28,7 +28,7 @@ async def get_all_for_user(current_user: User = Depends(get_current_user), db: S
     return service.get_all_for_user(current_user=current_user, db=db)
 
 
-@api_router.put("/", response_model=ShareFileUser)
+@api_router.put("", response_model=ShareFileUser)
 async def update_share(share: ShareFileUserUpdate,
                  current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
@@ -41,7 +41,7 @@ async def update_share(share: ShareFileUserUpdate,
         raise HTTPException(status_code=404, detail=str(e))
 
 
-@api_router.get("/{share_id}/", response_model=ShareFileUser)
+@api_router.get("/{share_id}", response_model=ShareFileUser)
 async def get_share(share_id: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
         return service.get_by_id(current_user=current_user, share_id=share_id, db=db)
@@ -53,7 +53,7 @@ async def get_share(share_id: int, current_user: User = Depends(get_current_user
         raise HTTPException(status_code=404, detail=str(e))
 
 
-@api_router.get("/forfile/{file_id}/", response_model=list[ShareFileUser])
+@api_router.get("/forfile/{file_id}", response_model=list[ShareFileUser])
 async def get_all_for_file(file_id: int,current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
         return service.get_all_for_file(file_id=file_id,current_user=current_user, db=db)
@@ -61,7 +61,7 @@ async def get_all_for_file(file_id: int,current_user: User = Depends(get_current
         raise HTTPException(status_code=404, detail=str(e))
 
 
-@api_router.delete("/{share_id}/", status_code=204)
+@api_router.delete("/{share_id}", status_code=204)
 async def delete_share(share_id: int, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     try:
         service.delete(current_user=current_user, share_id=share_id, db=db)
